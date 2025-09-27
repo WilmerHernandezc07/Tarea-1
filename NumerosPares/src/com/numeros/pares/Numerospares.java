1ff5d3bed667fdbcdb8be5aed2b6e57b056ae2b9
WilmerHernandezc07 <wilmerhernandezc07@gmail.com>
2025-09-27 15:22:38 -0400

    Agrego NumerosPares completo

---
+++ NumerosPares/src/com/numeros/pares/Numerospares.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.numeros.pares;
+
+/**
+ *
+ * @author Owner
+ */
+public class Numerospares {
+    public static void main(String[] args) {
+        int i = 2;
+        while( i <= 100 ) {
+        System.out.println(i);
+        i += 2;
+        }
+        
+    }
+    
+}
