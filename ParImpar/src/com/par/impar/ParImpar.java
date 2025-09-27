2294c0a94c16f07387b6814ac64d11d34b12e454
WilmerHernandezc07 <wilmerhernandezc07@gmail.com>
2025-09-27 15:24:16 -0400

    Agrego ParImpar completo

---
+++ ParImpar/src/com/par/impar/ParImpar.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.par.impar;
+
+/**
+ *
+ * @author Owner
+ */
+public class ParImpar {
+    public static void main(String[] args) {
+        int num = 21;
+        
+        if ( num % 2 == 0) {
+            System.out.println("El numero " +num+ " es Par");
+        }else {
+            System.out.println("El numero " +num+ " es Impar");
+        }
+    }
+    
+}
