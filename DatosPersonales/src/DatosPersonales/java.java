0bc924340bd09d6d8bf2104959afa3a10b2cb7d0
WilmerHernandezc07 <wilmerhernandezc07@gmail.com>
2025-09-27 15:34:58 -0400

    Agrego DatosPersonales completo

---
+++ DatosPersonales/src/DatosPersonales/java.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DatosPersonales;
+
+/**
+ *
+ * @author Owner
+ */
+public class java {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Wilmer");
+        System.out.println("wilmerhernandezc07@gmail.com");
+    }
+
+    
+}
